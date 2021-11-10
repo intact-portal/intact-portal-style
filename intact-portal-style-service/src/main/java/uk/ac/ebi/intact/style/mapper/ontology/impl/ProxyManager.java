c757f653641ff68af8240e78c733f841b399fd62
anjali <anjali@ebi.ac.uk>
2021-11-10 14:56:02 +0000

    NIV-728: Added ves-hx-47 proxy hosts

--- intact-portal-style-service/src/main/java/uk/ac/ebi/intact/style/mapper/ontology/impl/ProxyManager.java
+++ intact-portal-style-service/src/main/java/uk/ac/ebi/intact/style/mapper/ontology/impl/ProxyManager.java
@@ -14,14 +14,25 @@ public class ProxyManager {
 
     private static final Log log = LogFactory.getLog(TaxonMapper.class);
 
-    private static final String HTTPS_PROXY_HOST="HTTPS_PROXY_HOST";
-    private static final String HTTPS_PROXY_PORT="HTTPS_PROXY_PORT";
+    private static final String HTTPS_PROXY_HOST = "HTTPS_PROXY_HOST";// kubernetes
+    private static final String HTTPS_PROXY_PORT = "HTTPS_PROXY_PORT";// kubernetes
+    private static final String HTTP_PROXY_HOST = "HTTP_PROXY_HOST";// hinxton machine
+    private static final String HTTP_PROXY_PORT = "HTTP_PROXY_PORT";//hinxton machine
     private static Proxy proxy = Proxy.NO_PROXY;
 
     @Autowired
     private ProxyManager(Environment env) {
-        String proxyHost = env.getProperty(HTTPS_PROXY_HOST);
-        String proxyPort = env.getProperty(HTTPS_PROXY_PORT);
+
+        String proxyHost = null;
+        String proxyPort = null;
+
+        if (env.getProperty(HTTPS_PROXY_HOST) != null && env.getProperty(HTTPS_PROXY_PORT) != null) {
+            proxyHost = env.getProperty(HTTPS_PROXY_HOST);
+            proxyPort = env.getProperty(HTTPS_PROXY_PORT);
+        } else if (env.getProperty(HTTP_PROXY_HOST) != null && env.getProperty(HTTP_PROXY_PORT) != null) {
+            proxyHost = env.getProperty(HTTP_PROXY_HOST);
+            proxyPort = env.getProperty(HTTP_PROXY_PORT);
+        }
 
         if (proxyHost != null && proxyPort != null) {
             proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, Integer.parseInt(proxyPort)));
@@ -29,7 +40,7 @@ public class ProxyManager {
     }
 
     public Proxy getProxy() {
-        if(proxy.address() != null) {
+        if (proxy.address() != null) {
             log.info("Proxy address:" + proxy.address().toString());
         }
         return proxy;
